a46a608cd14b42aee7d5312a6d10df27bf4643c0
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-01-13 22:53:06 +0500

    setting up Pages, and React Routes, part-1

--- movix-webapp-reactjs18/src/App.jsx
+++ movix-webapp-reactjs18/src/App.jsx
@@ -1,16 +1,29 @@
 // Third party imports
 import {useSelector, useDispatch} from 'react-redux'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+// importing BrowserRouter from react-router-dom
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 import { fetchDataFromAPI } from "./utils/api"
 
 
 // redux-actions homeSLice
-import {getApiConfiguration, getGenres} from './store/homeSlice'
+import { getApiConfiguration, getGenres } from './store/homeSlice'
+
+// importing our (components & pages)
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Home from "./pages/home/Home"
+import Details from "./pages/details/Details"
+import SearchResult from "./pages/searchresult/SearchResult"
+import NotFound from "./pages/404/NotFound"
+import Explore from "./pages/explore/Explore"
 
 
 function App() {
-	const { url } = useSelector(state => state.home);
+	const { url, genres } = useSelector(state => state.home);
 	const dispatch = useDispatch();
 
 	// Testing my api, fetcher function
@@ -28,9 +41,20 @@ function App() {
 		apiTesting();
 	}, [])
 
-	return <div className="App">Hello, Musa
-	{url?.total_pages}
-	</div>;
+	return (
+		// This BrowserRouter acts as a wrapper for our App.jsx - all Routes will nested inside this
+		<BrowserRouter>
+			
+			{/* All the routes will be defined here */}
+			<Routes>
+				<Route path={"/"} element={<Home />} />
+				<Route path={"/:mediaType/:id"} element={<Details />} />
+				<Route path={"/search/:query"} element={<SearchResult />} />
+				<Route path={"/explore/:mediaType"} element={<Explore />} />
+				<Route  path={"*"} element={<NotFound />} />
+		</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ movix-webapp-reactjs18/src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import "./style.scss"
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
