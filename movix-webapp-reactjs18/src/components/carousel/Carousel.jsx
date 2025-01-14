8da3bc32aa81e938147f39943615e2bc86737f36
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-01-15 00:38:11 +0500

    trending section completed, part-1

--- movix-webapp-reactjs18/src/components/carousel/Carousel.jsx
+++ movix-webapp-reactjs18/src/components/carousel/Carousel.jsx
@@ -19,10 +19,78 @@ import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 
 
-const Carousel = () => {
+
+const Carousel = ({data, loading}) => {
+  // destructuring "url" from redux homeSlice
+  const { url } = useSelector(state => state.home);
+  const navigate = useNavigate();
+
+
+  // carousel horizontal scrool 4 cards per scroll
+  function navigation(dir) {
+    console.log(dir);
+  }
+
+  // function that return jsx code for skeleton Carousel - card
+  const skItem = () => {
+    return (
+      <div className="skeletonItem">
+        <div className="posterBlock skeleton"></div>
+        <div className="textBlock">
+          <div className="title skeleton"></div>
+          <div className="date skeleton"></div>
+        </div>
+      </div>
+    );
+  };
   return (
-    <div>Carousel</div>
-  )
+    <div className="carousel">
+      <ContentWrapper>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRightNav arrow"
+          onClick={() => navigation("right")}
+        />
+
+        {/* now rendering the card for carousel */}
+        {!loading ? (
+          <div className="carouselItems">
+            {data?.map((item, index) => {
+              const posterUrl = item?.poster_path
+                ? url.poster + item?.poster_path
+                : PosterFallback;
+              return (
+                <div key={item?.id} className="carouselItem">
+                  <div className="posterBlock">
+                    <Img src={posterUrl} />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">{item?.title || item?.name}</span>
+                    <span className="date">
+                      {dayjs(item?.release_Date || item?.first_air_date).format(
+                        "MMM D, YYYY"
+                      )}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+            <div className="loadingSkeleton">
+              {skItem()}
+              {skItem()}
+              {skItem()}
+              {skItem()}
+              {skItem()}
+          </div>
+        )}
+      </ContentWrapper>
+    </div>
+  );
 }
 
 export default Carousel
